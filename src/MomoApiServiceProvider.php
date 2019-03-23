f46b46a0de8973a3052bb05c45912375a33b59a3
linlak <nowomukama.linlak.linus@gmail.com>
2019-03-23 10:04:38 -0700

    initial push

---
+++ src/MomoApiServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace LaMomo\MomoApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class MomoApiServiceProvider extends ServiceProvider
+{
+	
+	public function boot()
+	{
+		
+	}
+	public function register()
+	{
+		# code...
+	}
+}
